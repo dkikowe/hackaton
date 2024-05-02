b3b554570fe1928fc7e57d340473c46a07d6974e
dkikowe <dkikowe@gmail.com>
2024-05-03 00:47:45 +0600

    иин с любой фотки

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import easyocr
 import cv2
 
-def textRecognition(file_path, coordinates):
-    reader = easyocr.Reader(["ru"], gpu = False)
+
+file_path = "img_3.png"
+def textRecognition(file_path):
+    reader = easyocr.Reader(["ru"], gpu = True , verbose = False)
     image = cv2.imread(file_path)
-    x1, y1 = coordinates[0]
-    x2, y2 = coordinates[2]
-    cropped_image = image[y1:y2, x1:x2]
-    result = reader.readtext(cropped_image, detail= 0)
+    result = reader.readtext(image, detail= 0)
     return result
 
+def birthdayRecognition():
+    for info in textRecognition(file_path):
+        if "." in info:
+            return info
+
+def iinRecognition():
+    for info in textRecognition(file_path):
+        if info.isdigit() and len(info) == 12:
+            return info
+
 def main():
-    file_path = "img.png"
-    coordinates = ([[115, 503], [321, 503], [321, 539], [115, 539]])
-    print(textRecognition(file_path=file_path, coordinates=coordinates))
+    print(iinRecognition(), birthdayRecognition())
 
 if __name__ == "__main__":
     main()
